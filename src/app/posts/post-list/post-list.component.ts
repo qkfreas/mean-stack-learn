1bf7d341858fb2af6295345d86cb92942d136fb0
Quinn Freas <quinnf@cdata.com>
2024-04-24 17:06:31 -0400

    Init

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css'],
+})
+export class PostListComponent {
+//   posts = [
+//     { title: 'First Post', content: 'This is post content' },
+//     { title: 'Second Post', content: 'This is post content' },
+//     { title: 'Third Post', content: 'This is post content' },
+//   ];
+    posts = [];
+}
