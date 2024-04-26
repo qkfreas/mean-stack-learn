fcac175f037740c33b3c68b96fe0b6d9dde01d46
Quinn Freas <quinnf@cdata.com>
2024-04-26 11:19:04 -0400

    Added posts service. Cleaned up code.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,9 @@
 import { Component } from '@angular/core';
 
-import { Post } from './posts/posts.model';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  storedPosts: Post[] = [];
-
-  onPostAdded(post) {
-    this.storedPosts.push(post);
-  }
 }
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,17 +1,34 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 import { Post } from '../posts.model';
+import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css'],
 })
-export class PostListComponent {
+export class PostListComponent implements OnInit, OnDestroy {
 //   posts = [
 //     { title: 'First Post', content: 'This is post content' },
 //     { title: 'Second Post', content: 'This is post content' },
 //     { title: 'Third Post', content: 'This is post content' },
 //   ];
-    @Input() posts = [];
+    posts: Post[] = [];
+    private postsSub: Subscription;
+
+    constructor(public postsService: PostsService) {}
+
+    ngOnInit(): void {
+        this.posts =this.postsService.getPosts();
+        this.postsSub = this.postsService.getPostUpdateListener()
+          .subscribe((posts: Post[]) => {
+            this.posts = posts;
+          });
+    }
+
+    ngOnDestroy(): void {
+        this.postsSub.unsubscribe();
+    }
 }
